36d6bfc4bc2562f69b5da7bc01f49f8d22e2de13
Kay <pnkhoa2609@gmail.com>
2020-03-26 09:40:22 +0700

    K3D v0.0.2

--- src/main.js
+++ src/main.js
@@ -1,31 +1,30 @@
-time = 0;
+const canvas = document.getElementById("canvas");
+canvas.width = 500;
+canvas.height = 500;
 
 let k3d = new K3D();
-k3d.createContext("canvas", 600, 600);
 
-let scene = new k3d.Scene(k3d._gl);
-let camera = new k3d.Camera();
-camera.lookAt(0, 0, 0);
+let scene = new k3d.Scene();
+let camera = new k3d.PerspectiveCamera(45, 1, 1, 200);
 
-scene.useCamera(camera);
 
-var box = new SceneObject();
-var box_geometry = k3d.BoxGeometry(3);
+
+var box_geometry = k3d.BoxGeometry(15);
 var normal_material = k3d.NormalMaterial([0.2, 0.3, 0.1, 1]);
 
-box.addGeometry(box_geometry);
-box.addMaterial(normal_material);
-
+var box = new k3d.Mesh(box_geometry, normal_material);
 scene.add(box);
 
-box.translateZ = 1;
+var renderer = new k3d.RenderEngine(canvas);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    box.rotate.x += 1.2;
 
 
-// function animate() {
-//     requestAnimationFrame(animate);
-//     scene.render();
-//     box.rotateX += 1.5;
-//     box.rotateZ += 1.5;
-// }
 
-// animate();
+    renderer.render(scene, camera);
+}
+
+animate();
