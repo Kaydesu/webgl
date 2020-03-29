c8cceb5aee9785f75e5e1b3318e0d7e90eeaa300
Kay <pnkhoa2609@gmail.com>
2020-03-29 19:51:54 +0700

    ON HANGEDgit add .

--- src/main.js
+++ src/main.js
@@ -1,32 +1,52 @@
 let scene, camera, renderer;
-let box_geometry, material1, material2, box, box2;
+let box_geometry, material1, material2, box, box2, cone, cone_geometry, triangle, triangle_geometry;
 let sphere_geometry, sphere;
 let time = 0;
-
-let k3d = new K3D();
 
 const canvas = document.getElementById("canvas");
 canvas.width = 500;
 canvas.height = 500;
 
 
+
+
 function init() {
-    scene = new k3d.Scene();
-    camera = new k3d.PerspectiveCamera(45, 1, 1, 1000);
+    scene = new K3D.Scene();
+    camera = new K3D.PerspectiveCamera(45, 1, 1, 1000);
     camera.eye.z = 120;
 
-    material1 = k3d.NormalMaterial([0.2, 0.3, 0.1, 1]);
-    material2 = k3d.NormalMaterial([0.2, 0.2, 0.4, 1])
+    material1 = K3D.BasicMaterial([
+        [0.2, 0.3, 0.1],
+        [0.5, 0.5, 0.2],
+        [0.1, 0.2, 0.3],
+        [0.2, 0.2, 0.4],
+        [0.3, 0.8, 0.2]
+    ], SHADE_OFF);
+    material2 = K3D.BasicMaterial([0.2, 0.2, 0.4, 1])
 
+    // box_geometry = K3D.BoxGeometry(25, 15, 30);
+    // box = new K3D.Mesh(box_geometry, material2);
+    // box.render_line = true;
 
-    sphere_geometry = k3d.UVSphereGeometry(20, 24, 24);
-    sphere = new k3d.Mesh(sphere_geometry, material1);
-    sphere.render_line = true;
+    // sphere_geometry = K3D.UVSphereGeometry(20, 24, 24);
+    // sphere = new K3D.Mesh(sphere_geometry, material1);
+    // sphere.render_line = true;
 
-    scene.add(sphere);
+    // cone_geometry = K3D.ConeGeometry(25, 45, 100);
+    // cone = new K3D.Mesh(cone_geometry, material2);
+    // cone.line_color = [0, 0, 0, 1];
+    // cone.render_line = true;
+    triangle_geometry = K3D.Triangle(
+        [0, 15, 25],
+        [-15, -15, 25],
+        [15, -15, 25]
+    );
+
+    triangle = new K3D.Mesh(triangle_geometry, material1);
+    // scene.add(triangle);
 
     // sphere.rotate.x = -90;
-    renderer = new k3d.RenderEngine(canvas);
+    renderer = new K3D.RenderEngine(canvas);
 }
 
 function eventUpdate() {
@@ -48,4 +68,4 @@ function animate() {
 }
 
 init();
-animate();
+// animate();
